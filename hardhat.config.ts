65d2f30f8c0f95bf6f1bdd51cb22922993336f37
张俊凯 <250336272@qq.com>
2023-12-14 20:46:44 +0800

    fix: 智能合约验证

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const { ETH_SEPOILA_URL, PRIVATE_KEY = "" } = process.env;
+const { ETH_SEPOILA_URL, PRIVATE_KEY = "", ETHERSCAN_APIKEY } = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
@@ -13,6 +13,12 @@ const config: HardhatUserConfig = {
       url: ETH_SEPOILA_URL,
       accounts: [PRIVATE_KEY],
     },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_APIKEY,
+  },
+  sourcify: {
+    enabled: true,
   },
 };
 
